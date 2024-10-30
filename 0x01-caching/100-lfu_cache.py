dab2c5db8049a646f227be78604b1527d64a47b6
jaskieerr <nizar.zairat@gmail.com>
2024-10-30 11:41:04 +0000

    hotfixing the hotfix

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -9,7 +9,7 @@ class LFUCache(BaseCaching):
     def __init__(self):
         '''LFU Caching'''
         super().__init__()
-        self.counter = {}
+        self.usage = {}
 
     def put(self, k, v):
         '''LFU Caching'''
@@ -18,29 +18,21 @@ class LFUCache(BaseCaching):
 
         if k in self.cache_data:
             self.cache_data[k] = v
-            self.counter[k] += 1
+            self.usage[k] += 1
         else:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                m_use = min(self.counter.values())
-                least_used_keys = [
-                    x for x, count in self.counter.items() if count == m_use
-                ]
-                lfu_key = min(least_used_keys, key=self.counter.get)
+                min_use = min(self.usage.values())
+                lfu_keys = [x for x, u in self.usage.items() if u == min_use]
+                lfu_key = min(lfu_keys, key=self.usage.get)
                 self.cache_data.pop(lfu_key)
-                self.counter.pop(lfu_key)
+                self.usage.pop(lfu_key)
                 print("DISCARD:", lfu_key)
 
             self.cache_data[k] = v
-            self.counter[k] = 1
+            self.usage[k] = 1
 
     def get(self, k):
         '''LFU Caching'''
         if k in self.cache_data:
-            self.counter[k] += 1
+            self.usage[k] += 1
             return self.cache_data.get(k)
-
-    def get(self, key):
-        '''LFU Caching'''
-        if key in self.cache_data:
-            self.freq[key] += 1
-            return self.cache_data.get(key)
