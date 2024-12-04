ad3122f2a6782096b40e33d24886a2e6738df211
jaskieerr <nizar.zairat@gmail.com>
2024-12-04 15:18:03 +0000

    fingers cossed like very very crossed

---
+++ 0x03-queuing_system_in_js/khuza.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo later > dump.rdb
+echo later > 0-redis_client.js
+echo later > 1-redis_op.js
+echo later > 2-redis_op_async.js
+echo later > 4-redis_advanced_op.js
+echo later > 5-subscriber.js
+echo later > 5-publisher.js
+echo later > 6-job_creator.js
+echo later > 6-job_processor.js
+echo later > 7-job_creator.js
+echo later > 8-job.js
+echo later > 8-job.test.js
+echo later > 9-stock.js
+echo later > 100-seat.js
